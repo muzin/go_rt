ae347a18544549674b7bdc6f4680d8992997972f
Sirius <sirius1@aliyun.com>
2021-03-24 09:35:38 +0800

    1. 文字添加亮色

--- print/colorstr/colorstr.go
+++ print/colorstr/colorstr.go
@@ -21,6 +21,7 @@ import "fmt"
 // 37  	47	  白色
 
 const (
+	// 暗色
 	textBlack = iota + 30
 	textRed
 	textGreen
@@ -29,12 +30,21 @@ const (
 	textPurple
 	textCyan
 	textWhite
+
+	// 亮色
+	textLightBlack  = 90
+	textLightRed    = 91
+	textLightGreen  = 92
+	textLightYellow = 93
+	textLightBlue   = 94
+	textLightPurple = 95
+	textLightCyan   = 96
+	textLightWhite  = 97
 )
 
 func Black(str string) string {
 	return textColor(textBlack, str)
 }
-
 func Red(str string) string {
 	return textColor(textRed, str)
 }
@@ -57,6 +67,32 @@ func White(str string) string {
 	return textColor(textWhite, str)
 }
 
+func LightBlack(str string) string {
+	return textColor(textLightBlack, str)
+}
+func LightRed(str string) string {
+	return textColor(textLightRed, str)
+}
+func LightYellow(str string) string {
+	return textColor(textLightYellow, str)
+}
+func LightGreen(str string) string {
+	return textColor(textLightGreen, str)
+}
+func LightCyan(str string) string {
+	return textColor(textLightCyan, str)
+}
+func LightBlue(str string) string {
+	return textColor(textLightBlue, str)
+}
+func LightPurple(str string) string {
+	return textColor(textLightPurple, str)
+}
+func LightWhite(str string) string {
+	return textColor(textLightWhite, str)
+}
+
+// 文字上色
 func textColor(color int, str string) string {
 	return fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", color, str)
 }
--- print/colorstr/colorstr_test.go
+++ print/colorstr/colorstr_test.go
@@ -2,6 +2,7 @@ package colorstr
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -17,19 +18,26 @@ func TestColorString(t *testing.T) {
 		t.Logf(Purple("Purple"))
 		t.Logf(White("White"))
 
+		t.Logf(LightBlack("Light Black"))
+		t.Logf(LightRed("Light Red"))
+		t.Logf(LightYellow("Light Yellow"))
+		t.Logf(LightGreen("Light Green"))
+		t.Logf(LightCyan("Light Cyan"))
+		t.Logf(LightBlue("Light Blue"))
+		t.Logf(LightPurple("Light Purple"))
+		t.Logf(LightWhite("Light White"))
+
 		t.Logf("successfully")
 	})
 }
 
-func TestCustomColorString(t *testing.T) {
-	t.Run("Test Custom Color String", func(t *testing.T) {
+func TestSannerColorRange(t *testing.T) {
+	t.Run("Test Sanner Color Range", func(t *testing.T) {
 
 		var str string = "Hello World."
-
 		var color = 30
-
 		for i := color; i < 1000; i++ {
-			sprintf := fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", i, str)
+			sprintf := fmt.Sprintf("\x1b[0;%dm%s%s\x1b[0m", i, str, strconv.Itoa(i))
 			fmt.Println(sprintf)
 		}
 
